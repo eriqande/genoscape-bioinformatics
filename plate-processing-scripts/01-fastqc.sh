5c5295d8f68cc4c51773dde142e0b64655afeed9
Eric C. Anderson <eric.anderson@noaa.gov>
2016-10-29 05:18:58 -0700

    tweak fastqc script.

--- plate-processing-scripts/01-fastqc.sh
+++ plate-processing-scripts/01-fastqc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #$ -cwd
 #$ -V
-#$ -N rad
+#$ -N fastqc
+#$ -o fastqc.log
+#$ -e fastqc.error
 #$ -pe shared 1
-#$ -l highp,h_data=8G,time=100:00:00
-#$ -M kruegg@ucsc.edu,eric.anderson@noaa.gov
+#$ -l highp,h_data=2G,time=1:20:00
+#$ -M eric.anderson@noaa.gov
 #$ -m bea
 
 # This should be run from the top level of the Plate_X directory
@@ -20,40 +22,3 @@ source data-defs.sh # call this second so you can override program-defs.sh if yo
 zcat $READ1 | $FASTQC stdin
 
 
-
-###Uncompress the raw fastqs
-gunzip WIFL-Plate1_S28_L003_R1_001.fastq.gz
-gunzip WIFL-Plate1_S28_L003_R2_001.fastq.gz
-
-###For bestRAD only: flip read pairs that are reversed and trim extra "GG"
-perl ~/scripts/flip_trim_werrors.pl ~/scripts/barcodes.txt WIFL-Plate1_S28_L003_R1_001.fastq WIFL-Plate1_S28_L003_R2_001.fastq \
-	WIFL_Plate1_R1.fq WIFL_Plate1_R2.fq true 2
-
-###Recompress fastq. This isn't strictly necessary, but I did it to save space
-gzip WIFL_Plate1_R1.fq
-gzip WIFL_Plate1_R2.fq
-
-###This calls the newer g++ compiler, necessary for running the newer stacks version on Hoffman2
-source ~/scripts/mypath.sh
-
-###Make directory for demultiplexed fastq files
-mkdir demultiplexed
-
-###Run stacks to demultiplex and trim adapter sequences
-~/programs/stacks-1.39/process_radtags -i gzfastq -1 WIFL_Plate1_R1.fq.gz -2 WIFL_Plate1_R2.fq.gz \
-	-o ./demultiplexed -b WIFL1barcodes.txt -c -q -r -e sbfI --filter_illumina \
-	--adapter_1 GATCGGAAGAGCACACGTCTGAACTCCAGTC --adapter_2 CACTCTTTCCCTACACGACGCTCTTCCGATCT
-
-###Move orphaned reads to their own directory
-mkdir orphans
-mv demultiplexed/*rem* orphans 
-
-###Make directory for duplicate filtered reads
-mkdir dupfiltered
-cd demultiplexed
-
-###Filter duplicate reads from demultiplexed fastq files, creating new files within the dupfiltered directory
-for sample in `ls *.1.fq.gz | cut -f1 -d'.'`
-do
-~/programs/stacks-1.39/clone_filter -1 $sample.1.fq.gz -2 $sample.2.fq.gz -i gzfastq -o ../dupfiltered
-done
